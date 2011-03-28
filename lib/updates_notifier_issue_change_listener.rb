dd0ed7d9c789e33c385cafefabdf6b4336aeeafd
Ramesh Nair <r.nair@7city.com>
2011-03-28 16:47:46 +0100

    * Add flag for ignoring changes made through the REST API.

--- lib/updates_notifier_issue_change_listener.rb
+++ lib/updates_notifier_issue_change_listener.rb
@@ -2,8 +2,10 @@ require 'rubygems'
 
 class UpdatesNotifierIssueChangeListener < Redmine::Hook::Listener
   def controller_issues_edit_after_save(context={})
-    if context[:issue] and context[:journal]
-      UpdatesNotifier.send_issue_update(User.current.login, context[:issue].id, context[:journal])
+    if !Setting.plugin_redmine_updates_notifier[:ignore_api_changes] or !context[:params][:format] or 'xml' != context[:params][:format]
+      if context[:issue] and context[:journal]
+        UpdatesNotifier.send_issue_update(User.current.login, context[:issue].id, context[:journal])
+      end
     end
   end
 end
