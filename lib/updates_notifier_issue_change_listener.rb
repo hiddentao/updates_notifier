5a175294d99a059a5e60c8a1a9b805c661c7602a
Ramesh Nair <r.nair@7city.com>
2011-04-01 17:17:06 +0100

    Handle the controller_issues_bulk_edit_after_save hook so that we get triggered during bulk updates too.
    This is hook available in https://github.com/7city/redmine

--- lib/updates_notifier_issue_change_listener.rb
+++ lib/updates_notifier_issue_change_listener.rb
@@ -1,6 +1,9 @@
 require 'rubygems'
 
 class UpdatesNotifierIssueChangeListener < Redmine::Hook::Listener
+  def controller_issues_bulk_edit_after_save(context={})
+    controller_issues_edit_after_save(context)
+  end
   def controller_issues_edit_after_save(context={})
     if !Setting.plugin_redmine_updates_notifier[:ignore_api_changes] or !context[:params][:format] or 'xml' != context[:params][:format]
       if context[:issue] and context[:journal]
